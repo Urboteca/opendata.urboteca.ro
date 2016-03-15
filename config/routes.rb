e582f1a203f32cdf6d130666340d2df8b2ebf4dc
Maria Nita <maria.nita.dn@gmail.com>
2016-03-15 08:30:00 +0200

    Add RailsAdmin routes- delete by mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'base/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
